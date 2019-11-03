d64a9afcd91a694e60d8abab8fd632f4b4504477
bigDuro <darrylcrockett@gmail.com>
2019-11-03 11:41:25 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bootstrap Framework! (w/ React Strap)
+      </header>
+    </div>
+  );
+}
+
+export default App;
