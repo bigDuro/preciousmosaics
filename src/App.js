01ffef10b713e1424bafdaf490d61cbaf36b5e17
bigDuro <darrylcrockett@gmail.com>
2019-11-04 08:34:17 -0500

    added fontawesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         Bootstrap Framework! (w/ React Strap)
-      </header>
+      </div>
     </div>
   );
 }
